b5373dfb73ffa8aef23476e57fbb644a6da801f6
PUTRA-gblk <sodikinsby11@gmail.com>
2021-11-05 08:09:34 +0700

    My-BotV2

---
+++ shop/allpayment.js
@@ -0,0 +1,31 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY PutraGanzz
+
+*さ BANK*
+1. BRI 
+	ㅁ Gw Belom Buat
+		• PutraGanzz
+		
+*さ E-MONEY*
+1. GOPAY
+	ㅁ 0858 5472 1440
+		• PutraGanzz
+2. OVO
+	ㅁ 0858 5472 1440
+		• PutraGanzz
+3. DANA
+	ㅁ 0858 5472 1440
+		• PutraGanzz
+
+*さ PULSA*
+1. INDOSAT
+	ㅁ 0858 5472 1440
+2. TELKOMSEL
+	ㅁ 0823 3410 9988
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
